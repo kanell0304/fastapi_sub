eef819da68c5c9f499168eac80d61719a39ad6b8
gyeongjun <kanell0304@gmail.com>
2025-09-12 12:52:54 +0900

    edit db.py

--- database/db.py
+++ database/db.py
@@ -5,11 +5,14 @@ from typing import Generator
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+load_dotenv(dotenv_path=".env")
 
 # ✅ DATABASE_URL 우선순위: 환경변수 > .env (uvicorn --env-file .env 로 로드 가능)
 # 예) MySQL:  mysql+pymysql://user:password@localhost:3306/fastapi_db?charset=utf8mb4
 # 예) SQLite: sqlite:///./app.db
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
+# DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Engine & Session
 engine = create_engine(DATABASE_URL, pool_pre_ping=True, future=True)
